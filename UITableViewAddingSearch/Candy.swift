3fba632f2972054bfb747ff8e91328dcbe4c0b23
lokialice <lokialicehd@gmail.com>
2015-05-28 22:09:42 +0700

    Table views are used in all kinds of apps, and offering a search option is a nice touch for usability. With UISearchBar and the UISearchDisplayController, iOS provides much of the functionality out of the box so there’s no excuse for not using it. The ability to search a large table view is something that today’s users expect; when they find it isn’t present, they won’t be happy campers!

---
+++ UITableViewAddingSearch/Candy.swift
@@ -0,0 +1,14 @@
+//
+//  Candy.swift
+//  UITableViewAddingSearch
+//
+//  Created by Macbook Pro MD102 on 5/28/15.
+//  Copyright (c) 2015 Macbook Pro MD102. All rights reserved.
+//
+
+import Foundation
+
+struct Candy{
+    let category:String
+    let name:String
+}
